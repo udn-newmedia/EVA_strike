d6fe08a9cb5b8a5921f3f1d2973a9fdf71a474e6
lovebuizel <40226745+lovebuizel@users.noreply.github.com>
2019-06-21 21:07:57 +0800

    contentA+B update
    contentA+B update

--- src/components/ContentA.vue
+++ src/components/ContentA.vue
@@ -11,12 +11,12 @@
     <div class="container-gray">
       <Content style="background: transparent;">
         <h3 class="mt-3 mb-3">人在台灣出不去</h3>
-        <p class="colorBlue bold mt-3 mb-3">個人旅客：</p>
+        <p class="bold mt-3 mb-3">個人旅客：</p>
         <p>1. <span class="bold">可「免手續費」</span>辦理退改票或簽轉其他航班。</p>
         <p>2. 隨時留意機場廣播及航空公司公告的處理辦法，有食宿需求可向航空公司提出。</p>
         <p>3. 長榮航空已公告，若航班起飛時間延誤6小時以上，或簽轉它航班機起飛時間較原定行程延誤6小時以上者，長榮航空彌補的食宿上限金額為<span class="bold">美金250元</span>。</p>
-        <p>4. <span class="bold">食宿收據、更改後的航班登機證請保留</span>，事後上傳長榮官網「<a href="#" class="colorGreen text-decoration">交通/住宿費用申請專區</a>」申請補償費用。</p>
-        <p class="colorOrange bold mt-3 mb-3">團體旅客：</p>
+        <p>4. <span class="bold">食宿收據、更改後的航班登機證請保留</span>，事後上傳長榮官網「<a href="https://booking.evaair.com/flyeva/EVA/B2C/reimbursement.aspx?date=20190620-20190625&offdate=20190620-20190920" target="_blank" class="colorGreen text-decoration">交通/住宿費用申請專區</a>」申請補償費用。</p>
+        <p class="bold mt-3 mb-3">團體旅客：</p>
         <p>1. 旅行社應<span class="bold">安排旅客轉搭其他航班或轉團</span>。</p>
         <p>2. 衍伸的滯留費用應由<span class="bold">旅行社負擔</span>，旅行社事後可向航空公司求償。</p>
       </Content>
@@ -24,12 +24,12 @@
 
     <Content>
       <h3 class="mt-3 mb-3">人在海外被困住</h3>
-      <p class="colorBlue bold mt-3 mb-3">個人旅客：</p>
+      <p class="bold mt-3 mb-3">個人旅客：</p>
       <p>1. <span class="bold">可「免手續費」</span>辦理退改票或簽轉其他航班。</p>
       <p>2. 若有<span class="bold">食宿需求</span>可向航空公司提出，航空公司應協助處理。</p>
       <p>3. 長榮航空已公告，若航班起飛時間延誤6小時以上，或簽轉它航班機起飛時間較原定行程延誤6小時以上者，長榮航空彌補的食宿上限金額為<span class="bold">美金250元</span>。</p>
-      <p>4. <span class="bold">食宿收據、更改後的航班登機證請保留</span>，事後上傳長榮官網「<a href="#" class="colorGreen text-decoration">交通/住宿費用申請專區</a>」申請補償費用。</p>
-      <p class="colorOrange bold mt-3 mb-3">團體旅客：</p>
+      <p>4. <span class="bold">食宿收據、更改後的航班登機證請保留</span>，事後上傳長榮官網「<a href="https://booking.evaair.com/flyeva/EVA/B2C/reimbursement.aspx?date=20190620-20190625&offdate=20190620-20190920" target="_blank" class="colorGreen text-decoration">交通/住宿費用申請專區</a>」申請補償費用。</p>
+      <p class="bold mt-3 mb-3">團體旅客：</p>
       <p>1. 旅行社應<span class="bold">安排旅客轉搭其他航班或轉團</span>。</p>
       <p>2. 若無法依原訂行程返台，衍伸的滯留費用應由<span class="bold">旅行社負擔</span>。</p>
     </Content>
@@ -37,9 +37,9 @@
     <div class="container-gray">
       <Content style="background: transparent;">
         <h3 class="mt-3 mb-3">想未雨綢繆</h3>
-        <p class="colorBlue bold mt-3 mb-3">個人旅客：</p>
+        <p class="bold mt-3 mb-3">個人旅客：</p>
         <p>若欲搭乘的航班尚未受影響，退改票原則上依購買機票時的退改票規定辦理，<span class="bold">會有手續費」</span>。</p>
-        <p class="colorOrange bold mt-3 mb-3">團體旅客：</p>
+        <p class="bold mt-3 mb-3">團體旅客：</p>
         <p>洽各旅行社。但旅行社「易遊網」指出，尚未受影響的航班不確定性較大，建議<span class="bold">依照原訂日期和時間至機場集合</span>，以維護自身權益。</p>
       </Content>
     </div>
@@ -94,14 +94,19 @@
       </div>
       <p class="fz-15">資料來源：各產險公司 整理：Money101.com.tw金融產品比較平台</p>
 
-      <p class="mt-3 mb-3">3. 申請班機延誤證明：可利用長榮航空公司官網\<a href="#" class="colorGreen text-decoration">班機實際到離</a>\<a href="#" class="colorGreen text-decoration">航班離到場證明</a>功能自行列印，或聯繫長榮航空各地服務中心申請。</p>
+      <p class="mt-3 mb-3">3. 申請班機延誤證明：可利用長榮航空公司官網\<a href="https://booking.evaair.com/flyeva/EVA/B2C/flight-status.aspx" target="_blank" class="colorGreen text-decoration">班機實際到離</a>\<a href="https://booking.evaair.com/flyeva/EVA/B2C/flight-status.aspx?tab=4" target="_blank" class="colorGreen text-decoration">航班離到場證明</a>功能自行列印，或聯繫長榮航空各地服務中心申請。</p>
 
       <h3 class="mt-3 mb-3">有投保「旅遊不便險」者</h3>
       <p>1. 消基會長榮罷工申訴專線：02-27001234</p>
-      <p>2. 行政院消費者保護會網站<a href="#" class="colorGreen text-decoration">線上申訴</a></p>
+      <p>2. 行政院消費者保護會網站<a href="https://cpc.ey.gov.tw/Page/27091D3670230874" target="_blank" class="colorGreen text-decoration">線上申訴</a></p>
       <p>3. 團體旅客：檢附相關收據及契約以書面向旅行業品保協會（台北市民權東路2段9號5F）或觀光局（台北市忠孝東路4段290號9樓）申訴</p>
-      <p class="mt-3 mb-3"><span class="bold">更多資訊：</span>行政院消保處「<a href="#" class="colorGreen text-decoration">長榮航空罷工專區</a>」</p>
+      <p class="mt-3 mb-3"><span class="bold">更多資訊：</span>行政院消保處「<a href="https://cpc.ey.gov.tw/Page/27091D3670230874" target="_blank" class="colorGreen text-decoration">長榮航空罷工專區</a>」</p>
     </Content>
+    <div style="width:100%;text-align:center;" class="mb-5">
+      <div class="moreBlock">
+        <a href="https://udn.com/news/cate/2/6644" target="_blank">更多長榮罷工最新消息</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -135,6 +140,8 @@ hr
   margin-top: 3rem
 .mb-3
   margin-bottom: 3rem
+.mb-5
+  margin-bottom: 5rem
 .container-gray
   background-color: #eaeaea
   width: 100%
@@ -171,4 +178,13 @@ hr
   font-size: 15px
 .text-decoration
   text-decoration: underline
+.moreBlock
+  margin: 0 auto
+  background-color: #ff6f00
+  display: inline-block
+  padding: 2rem 4rem
+  border-radius: 5rem
+  a
+    color: #fff
+    font-size: 21px
 </style>
--- src/components/ContentB.vue
+++ src/components/ContentB.vue
@@ -1,18 +1,202 @@
 <template>
-  <div>
-    bbb
+  <div class="ContentB">
+    <Content>
+      <h2 class="mt-3 mb-3 text-align-center fz-40-50">長榮空服員為何要罷工？<br>5個QA一次看懂</h2>
+      <p>自2017年4月起，長榮航空與桃園空服員職業工會歷經20多次協商，仍然無助改善勞資關係，工會表示資方對案說法一模一樣，毫無進展，因此宣布6月20日下午4點全面罷工。</p>
+      <br>
+      <p>到底是什麼重大爭議，讓長榮陷入首次空服員罷工的危機中？長榮空服員在爭什麼？《聯合報》整理5大QA，帶你一次看懂長榮勞資爭議重點。</p>
+      <div class="mt-3 mb-3" style="width: 100%;">
+        <img :src="srcRWD('images/contentB_1_mob.jpg', 'images/contentB_1_pc.jpg')" alt="桃園空服員職業工會與長榮航空勞資協商破局，約千名空服員在長榮航運大樓前靜坐罷工。" title="桃園空服員職業工會與長榮航空勞資協商破局，約千名空服員在長榮航運大樓前靜坐罷工。" style="width: 100%;">
+        <p class="fz-16">桃園空服員職業工會與長榮航空勞資協商破局，約千名空服員在長榮航運大樓前靜坐罷工。</p>
+      </div>
+      <h3 class="mt-5 mb-2 text-align-center fz-40">Q1：訴求什麼？</h3>
+      <p>桃園市空服員職業工會提出8點訴求，包括：</p>
+      <br>
+      <p>1、日支費應調高為每小時新臺幣150元，非會員不得比照適用</p>
+      <p>2、東京、北京、金邊、胡志明、哈爾濱、瀋陽、呼和浩特等9條航線改為過夜班</p>
+      <p>3、開放工會參與會員的人評會等懲處機制，並有發言及表決權</p>
+      <p>4、工會參與推派獨立董事或增設勞工董事</p>
+      <p>5、國定假日出勤應給付兩倍工資</p>
+      <p>6、各航班派遣外籍組員不應超過2人</p>
+      <p>7、給予工會理監事、會員代表會務公假</p>
+      <p>8、變更空服員現有勞動條件與工作規則應事先與工會協商</p>
+
+      <h3 class="mt-5 mb-2 text-align-center fz-40">Q2：為什麼「日支費」很重要？</h3>
+      <p>桃空職工會強力要求長榮給付予空服員的「日支生活費」（簡稱日支費）比照華航，由每小時3美元（約新台幣90元）調高至5美元（約新台幣150元）。</p>
+      <br>
+      <p class="m-0">空服員薪資結構為「底薪」加上「飛行加給」與「日支費」。華航及長榮的日支費計算方式不同，華航給付日支費給空服員的期間，是從「起飛」計算到「降落」；長榮則是從「報到」計算到「報離」，長榮每趟會比華航多計算約2-3小時。</p>
+      <div class="block-orange"><p>資方怎麼說？</p></div>
+      <p>長榮給付日支費的時間比華航長（空服員報到起就開始給付日支費），所以整體日支費不比華航低。換算下來每位空服員平均每頓餐費約新台幣1600-2000元，「已優於一般國際行情」，另也針對該訴求增設「飛安服勤獎金」，讓空服員每月實質增加1500到2500元不等收入。</p>
+      <div class="mt-3 mb-3" style="width: 100%;">
+        <img :src="srcRWD('images/contentB_2_mob.png', 'images/contentB_2_pc.png')" alt="長榮VS.華航空服員待遇比一比" title="長榮VS.華航空服員待遇比一比" style="width: 100%;">
+      </div>
+
+      <h3 class="mt-5 mb-2 text-align-center fz-40">Q3：什麼是「禁搭便車條款」？</h3>
+      <p class="m-0">桃空職工會要求長榮航空同意「禁搭便車條款」，也就是非工會的成員，不得適用調高後的待遇。禁搭便車條款是長榮資方長期堅持不讓步的項目，也是此次協商破裂的主因。目前長榮航空空服員約4200人，約有3000人加入桃園空服員職業工會。</p>
+      <div class="block-orange"><p>資方怎麼說？</p></div>
+      <p>長榮資方表示，若同意「禁搭便車」會有飛安上的疑慮，讓同一航班上的組員有同工不同酬的怨懟，影響團隊和默契。必須重視加入其他工會和選擇不加入職業工會的聲音與權益。</p>
+
+      <h3 class="mt-5 mb-2 text-align-center fz-40">Q4：這9條航線為什麼要改成「過夜航班」？</h3>
+      <p class="m-0">桃空職工會訴求將3條台灣飛東京的航線，另外6條分別飛北京、金邊、胡志明、哈爾濱、瀋陽、呼和浩特，共9條航線改為過夜班。這幾條航線若遇上天候不佳、裝載超時，就有可能超過《勞基法》規定的「不得連續工作12小時」，因此希望改成「今天飛去、明天飛回」。</p>
+      <div class="block-orange"><p>資方怎麼說？</p></div>
+      <p>長榮公司強調，在台空勤組員皆需符合<a class="colorGreen text-decoration" href="https://law.moj.gov.tw/LawClass/LawSingle.aspx?pcode=K0090041&flno=37" target="_blank">《航空器飛航作業管理規則》（AOR）</a>及勞基法雙重規範，工會一開始訴求單一航段飛行時間(FT)超過3小時，就應在外站過夜，與現行國際標準「國際航線飛航時間不得超過10小時」落差太大。</p>
+      <br>
+      <p>針對改善辛勞航班部分，長榮表示，已提出「桃園-金邊」及「桃園-東京」改用專飛班，指1個月上班10天休20天、「桃園-瀋陽」冬季2組空服員輪值，「桃園-北京」6至8月雷雨季改為外站過夜班、「高雄-東京」冬季班表外站過夜。</p>
+
+      <h3 class="mt-5 mb-2 text-align-center fz-40">Q5：空服員爭勞權，跟「外籍空服員」有什麼關係？</h3>
+      <p class="m-0">桃空職工會訴求各航班派遣外籍組員不應超過2人，主要擔心航空公司大量聘用外籍空服員，給予較低薪資，來壓低人事成本，影響本國空服員的工作權。</p>
+      <div class="block-orange"><p>資方怎麼說？</p></div>
+      <p>長榮航空表示，考慮飛安，發生緊急狀況需90秒內撤離旅客，國際慣例需有熟悉母語的空服員服務。長榮外籍旅客占5成，有為數眾多的越、泰、日籍旅客，限制2名外籍組員不合理。</p>
+    </Content>
+    
+    <div class="container-gray">
+      <Content style="background: transparent;">
+        <h3 class="mt-3 mb-3 text-align-center fz-40">一張表看懂 長榮勞資爭議</h3>
+
+        <p class="bold fz-21-25">1.日支費調高</p>
+        <p class="bold">工會訴求：日支費應調高為每小時新臺幣150元，非會員不得比照適用</p>
+        <p class="colorGreen">長榮回應：</p>
+        <p>●日支費已夠，平均每餐1600-2000元</p>
+        <p>●另飛安服勤獎金提高至200元／時</p>
+        <p>●不同意禁搭便車</p>
+        <hr>
+
+        <p class="bold fz-21-25">2.改善疲勞航班</p>
+        <p class="bold">工會訴求：東京、北京、金邊、胡志明、哈爾濱、瀋陽、呼和浩特等9條航線改為過夜班</p>
+        <p class="colorGreen">長榮回應：</p>
+        <p>各航線已提出部份月份過夜、自願專飛、輪流值勤等方案</p>
+        <hr>
+
+        <p class="bold fz-21-25">3.參與人評會</p>
+        <p class="bold">工會訴求：開放本會參與會員的人評會等懲處機制，並有發言及表決權</p>
+        <p class="colorGreen">長榮回應：</p>
+        <p>當事組員已可邀一位陪同列席</p>
+        <hr>
+
+        <p class="bold fz-21-25">4.增設勞工董事</p>
+        <p class="bold">工會訴求：工會參與推派獨立董事或增設勞工董事</p>
+        <p class="colorGreen">長榮回應：</p>
+        <p>已請主管機關研究修法</p>
+        <hr>
+
+        <p class="bold fz-21-25">5.假日出勤兩倍薪資</p>
+        <p class="bold">工會訴求：國定假日出勤應給付兩倍工資</p>
+        <p class="colorGreen">長榮回應：</p>
+        <p>休假日與一般勞工均為116天、甚至更多，沒有2倍薪問題</p>
+        <hr>
+
+        <p class="bold fz-21-25">6.限制外籍組員人數</p>
+        <p class="bold">工會訴求：各航班派遣外籍組員不應超過2人</p>
+        <p class="colorGreen">長榮回應：</p>
+        <p>長榮航空外籍旅客占5成，緊急狀況需在90秒內撤離旅客，國際慣例需有熟悉母語空服員服務</p>
+        <hr>
+
+        <p class="bold fz-21-25">7.給予會務公假</p>
+        <p class="bold">工會訴求：給予本會理監事、會員代表會務公假</p>
+        <p class="colorGreen">長榮回應：</p>
+        <p>給予每年申請25天會務假</p>
+        <hr>
+
+        <p class="bold fz-21-25">8.變更勞動條件須協商</p>
+        <p class="bold">工會訴求：變更空服員現有勞動條件與工作規則前，應先與工會協商</p>
+        <p class="colorGreen">長榮回應：</p>
+        <p>已有組員月會議、事務長會議、「我有話要說」等平台</p>
+        <hr>
+
+        <p class="fz-15">資料來源：長榮航空、桃園空服員職業工會</p>
+        <p class="fz-15">製表：侯俐安</p>
+
+      </Content>
+    </div>
+    <div style="width:100%;text-align:center;" class="mt-5 mb-5">
+      <div class="moreBlock">
+        <a href="https://udn.com/news/cate/2/6644" target="_blank">更多長榮罷工最新消息</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Content from '@/components/Content.vue';
 export default {
   name: 'ContentB',
   components: {
-
+    Content
+  },
+  methods: {
+    srcRWD( mob, pc ) {
+      if(window.innerWidth<768) {
+        return mob;
+      }else {
+        return pc;
+      }
+    }
   }
 }
 </script>
 
-<style>
-
+<style scoped lang="sass">
+hr
+  border: none
+  border-top: 1px solid #000
+.text-align-center
+  text-align: center
+.colorGreen
+  color: #00832c
+.text-decoration
+  text-decoration: underline
+.bold
+  font-weight: bold
+.m-0
+  margin: 0
+.mb-2
+  margin-bottom: 2rem
+.mt-3
+  margin-top: 3rem
+.mb-3
+  margin-bottom: 3rem
+.mt-5
+  margin-top: 5rem
+.mb-5
+  margin-bottom: 5rem
+.block-orange
+  margin: 2rem 0 2rem 0
+  display: inline-block
+  background-color: #ff6f00
+  padding: 3px 24px
+  p
+    margin: 0
+    color: #fff
+.fz-15
+  font-size: 15px
+.fz-16
+  font-size: 16px
+.fz-40-50
+  font-size: 40px
+  line-height: 1.18
+  @media (min-width: 768px)
+    font-size: 50px
+    line-height: 1.29
+.fz-40
+  font-size: 40px
+  line-height: 1.18
+  text-align: left
+  @media (min-width: 768px)
+    text-align: center
+.fz-21-25
+  font-size: 21px
+  @media (min-width: 768px)
+    font-size: 25px
+.container-gray
+  background-color: #eaeaea
+  width: 100%
+.moreBlock
+  margin: 0 auto
+  background-color: #ff6f00
+  display: inline-block
+  padding: 2rem 4rem
+  border-radius: 5rem
+  a
+    color: #fff
+    font-size: 21px
 </style>
